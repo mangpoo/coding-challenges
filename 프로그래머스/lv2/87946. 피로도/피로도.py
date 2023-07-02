2850cdf9a8b0915e3d03f2dd2883ee050ba65e17
Seungwoon Kim <128568226+mangpoo@users.noreply.github.com>
2023-07-02 17:09:36 +0900

    [level 2] Title: 피로도, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,19 @@
+from itertools import *
+
+def solution(k, dungeons):
+    
+    temp = -1
+
+    sort_dungeons = list(permutations(dungeons, len(dungeons)))
+    count = [0] * len(sort_dungeons)
+
+    for i in sort_dungeons:
+        temp += 1
+        temp_k = k
+        for j in range(len(i)):
+            if(temp_k >= i[j][0]):
+                temp_k = temp_k - i[j][1]
+                count[temp] += 1
+    
+        
+    return max(count)
