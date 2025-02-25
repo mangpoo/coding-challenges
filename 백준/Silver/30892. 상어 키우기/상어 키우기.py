c226cd698eb098e53b2120822fc23ffe0094c167
Seungwoon Kim <128568226+mangpoo@users.noreply.github.com>
2025-02-25 19:28:16 +0900

    [Silver I] Title: 상어 키우기, Time: 212 ms, Memory: 56452 KB -BaekjoonHub

--- 백준/Silver/30892. 상어 키우기/상어 키우기.py
+++ 백준/Silver/30892. 상어 키우기/상어 키우기.py
@@ -1,5 +1,5 @@
-import sys
 # 아기상어
+import sys
 
 N, K, T = map(int, sys.stdin.readline().split())
 shark = list(map(int, sys.stdin.readline().split()))
@@ -10,12 +10,12 @@ count = 0
 for _ in range(K):
 
     while count < N and shark[count] < T:
-        eat_shark.append(shark[count])  
-        count += 1  
+        eat_shark.append(shark[count])
+        count += 1
 
-    if not eat_shark:
-        break
+    if eat_shark:
+        T += eat_shark.pop()
 
-    T += eat_shark.pop()
+
 
 print(T)
